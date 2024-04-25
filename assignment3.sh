725226c9015a9571bacc47700030e6ac8c82a5f1
adamroh <40237698+adamroh@users.noreply.github.com>
2024-04-25 15:13:51 -0400

    Update assignment3.sh

--- assignment3.sh
+++ assignment3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Welcome Rohan!"
-sed 's/zoo//I' pandas.txt ">" panda_zoos.txt
+sed 's/zoo//I' pandas.txt > panda_zoos.txt
 grep -v 'Name' pandas.txt | sort -k2 | awk '{print $1, $4, $2}' > sorted_pandas.txt
 cut -f5 pandas.txt | sort > panda_zoos_A2Z.txt
-sed 's/male/M/g; s/female/F/g' pandas.txt > panda_m_f.txt
-grep -c 'female' pandas.txt | grep 'october'
+sed 's/male/M/g: s/female/F/g' pandas.txt > panda_m_f.txt
+grep 'october' | grep -c 'female' pandas.txt
 cat -n pandas.txt > panda_names.txt
 sed -n '1p;6p;17p' pandas.txt > lines_1_6_17.txt
 grep -i 'x' pandas.txt > x_lines.txt
